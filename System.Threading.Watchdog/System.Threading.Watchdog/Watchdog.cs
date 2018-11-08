a98ac5af58cfc60a92fdca5ec6724ee7dd69b56f
Benjamin Saggin <benjamin.saggin@live.fr>
2018-11-08 11:50:01 +0100

    [Add] First commit

---
+++ System.Threading.Watchdog/System.Threading.Watchdog/Watchdog.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+
+namespace System.Threading.Watchdog
+{
+    public static class Watchdog
+    {
+        /// <param name="action"></param>
+        /// <param name="waitTime">Time allocated to the action to run before the watchdog barks</param>
+        /// <returns>true if the watchdog didn't bark, else false</returns>
+        public static bool ExecuteWithWatchdog(Action action, TimeSpan waitTime)
+        {
+            bool result = false;
+            AutoResetEvent watchdog = new AutoResetEvent(false);
+            Task.Factory.StartNew(() =>
+            {
+                action.Invoke();
+                watchdog.Set();
+                result = true;
+            });
+            watchdog.WaitOne(waitTime);
+            return result;
+        }
+    }
+}
